664c9937bf3c87dd52375385d27f580936afcc53
dem0saic <changeinculture4lyf@proton.me>
2023-10-09 19:12:10 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - this is the main func
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+		putchar('\n');
+		return (0);
+}
