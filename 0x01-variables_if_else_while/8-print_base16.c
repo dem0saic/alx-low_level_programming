c7692681c52c8ee69ace3cbc76dde1f3376110d4
dem0saic <changeinculture4lyf@proton.me>
2023-10-09 19:20:41 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - this is the main func
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+	int num = 0;
+
+	while
+		(num < 10) {
+			putchar(num + '0');
+			num++;
+		}
+	while
+		(letter < 'g') {
+			putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+		return (0);
+}
