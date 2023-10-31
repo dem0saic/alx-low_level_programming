7518691401705e14af40363709d8ad2092e07fb6
dem0saic <changeinculture4lyf@proton.me>
2023-10-31 15:10:42 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 if successful, 1 if an error occurs
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
